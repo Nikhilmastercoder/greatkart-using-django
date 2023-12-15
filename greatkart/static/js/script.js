39dcf4a23fd5a2bbc7e7138608524db3988ebf49
Nikhil <niksac4910@gmail.com>
2023-12-16 02:27:33 +0530

    django authentication

--- greatkart/static/js/script.js
+++ greatkart/static/js/script.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
 
     /* ///////////////////////////////////////
 
-    THESE FOLLOWING SCRIPTS ONLY FOR BASIC USAGE, 
+    THESE FOLLOWING SCRIPTS ONLY FOR BASIC USAGE,
     For sliders, interactions and other
 
     */ ///////////////////////////////////////
-    
+
 
 	//////////////////////// Prevent closing from click inside dropdown
     $(document).on('click', '.dropdown-menu', function (e) {
@@ -44,17 +44,14 @@ $(document).ready(function() {
         }
     });
 
-
-
 	//////////////////////// Bootstrap tooltip
 	if($('[data-toggle="tooltip"]').length>0) {  // check if element exists
 		$('[data-toggle="tooltip"]').tooltip()
 	} // end if
 
-
-
-
-    
-}); 
+});
 // jquery end
 
+setTimeout(function(){
+  $('#message').fadeOut('slow')
+}, 4000)
